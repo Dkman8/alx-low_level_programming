791e4ab8dc7ed0ff1c7af6eadc7a6f7ea0a5f413
Dkman8 <dkman571@gmail.com>
2022-09-08 16:24:16 -0700

    6. Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the sizeof many var types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        chair a;
+ 	int 
+	long int c;
+	long long int d;
+	float e;
+
+	pintf(Size of char: %d byte(s)\n" , sizeof(a));
+	pintf(Size of int: %d byte(s)\n" , sizeof(b));
+	pintf(Size of a long int: %d byte(s)\n" , sizeof(c));
+	pintf(Size of a long long int: %d byte(s)\n" , sizeof(d));
+	pintf(Size of a float: %d byte(s)\n" , sizeof(e));
+	return (0);
+}
