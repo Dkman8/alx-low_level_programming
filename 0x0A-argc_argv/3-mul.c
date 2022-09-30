99efcf3a6192849770f2cb4b2eef7328b485ebcd
Dkman8 <dkman571@gmail.com>
2022-09-30 02:44:23 -0700

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is a function to print its name
+ * @argc: argc parameter
+ * @argv: Array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
+
+	return (0);
+}
